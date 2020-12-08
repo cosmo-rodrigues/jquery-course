2050fa238c2ff91663a40869485e90213c9b562e
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-07 23:34:09 -0300

    Fisrt commit

---
+++ starting-course/script.js
@@ -0,0 +1,45 @@
+$(function() {
+  $('#text').text('Hello World!!!');
+
+  $('#sumButton').on('click', function() {
+    const number01 = parseInt($('#number01').val());
+    const number02 = parseInt($('#number02').val());
+    const result = number01 + number02;
+    if (result) $('#result').text(result);
+  });
+
+  $('#previousButton').text('<');
+  $('#nextButton').text('>');
+
+  const images = ['./img/anakin-vader.jpg', './img/leon.jpg', './img/umbrela-monkie.jpg', '/img/yoda.jpg'];
+
+  const maxImages = images.length - 1;
+  let index = 0;
+
+  $('#banner').css('background-image', `url(${images[index]})`);
+
+  function onChange(optin) {
+    $('#banner')
+      .css('background-image', `url(${images[index]})`)
+      .fadeOut(500, function() {
+        index += optin;
+        if(index > maxImages) {
+          index = 0;
+        } else if(index < 0){
+          index = maxImages;
+        }
+        $('#banner')
+          .css('background-image', `url(${images[index]})`)
+          .fadeIn(500);
+      });
+  }
+
+  $('#previousButton').on('click', function() {
+    onChange(-1);
+  });
+
+  $('#nextButton').on('click', function() {
+    onChange(1);
+  });
+
+});
